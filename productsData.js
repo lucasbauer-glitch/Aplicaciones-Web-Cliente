24ecb832cd79c20a515258ee3eec70a7eeb920e6
Lucas <lucas.bauer@istea.com.ar>
2025-09-13 20:58:48 -0300

    se crea js que contenga el array de objetos para ser importada

---
+++ productsData.js
@@ -0,0 +1,68 @@
+const products = [
+  {
+    link: "https://www.shibuyacomicstore.com.ar/productos/pokemon-booster-pack-sv-journey-together-ingles-arte-aleatorio/",
+    title: "Pokémon Booster Pack S&V Surging Sparks Inglés (Arte Aleatorio)",
+    images: [
+       "https://acdn-us.mitiendanube.com/stores/001/989/991/products/327px-sv8_booster_pikachu-2c0ce094f7dff2d2ba17398482200736-1024-1024.webp"
+      ],
+    priceCurrent: "$7.650,00",
+    priceOld: "$8.500,00",
+    discount: "-10% OFF",
+    id: "199652512"
+  },
+  {
+    link: "https://www.shibuyacomicstore.com.ar/productos/pokemon-booster-pack-sv-journey-together-ingles-arte-aleatorio/",
+    title: "Pokémon Booster Pack S&V Obsidian Flames Inglés (Arte Aleatorio)",
+    images: [
+      "https://acdn-us.mitiendanube.com/stores/001/989/991/products/pokemon_tcg_scarlet_violetobsid-dd8fdb7a4964322d0a17451143424423-1024-1024.png"
+      ],
+    priceCurrent: "$7.650,00",
+    priceOld: "$8.500,00",
+    discount: "-10% OFF",
+    id: "199652513"
+  },
+  {
+    link: "https://www.shibuyacomicstore.com.ar/productos/pokemon-booster-pack-sv-journey-together-ingles-arte-aleatorio/",
+    title: "Pokémon Booster Pack S&V Obsidian Flames Inglés (Arte Aleatorio)",
+    images: [
+       "https://acdn-us.mitiendanube.com/stores/001/989/991/products/pokemon_tcg_scarlet_violetobsid-2-11e0a9ba4c6917720617451143453318-1024-1024.png"
+    ],
+    priceCurrent: "$7.650,00",
+    priceOld: "$8.500,00",
+    discount: "-10% OFF",
+    id: "199652514"
+  },
+  {
+    link: "https://www.shibuyacomicstore.com.ar/productos/pokemon-booster-pack-sv-journey-together-ingles-arte-aleatorio/",
+    title: "Pokémon Booster Pack Sleeved S&V Stellar Crown Inglés (Arte Aleatorio)",
+    images: [
+       "https://acdn-us.mitiendanube.com/stores/001/989/991/products/pokemon_tcg_scarlet_violetparad-e4f5254e03feca6b0217398502377050-1024-1024.webp"
+    ],
+    priceCurrent: "$7.650,00",
+    priceOld: "$8.500,00",
+    discount: "-10% OFF",
+    id: "199652515"
+  },
+  {
+    link: "https://www.shibuyacomicstore.com.ar/productos/pokemon-booster-pack-sv-journey-together-ingles-arte-aleatorio/",
+    title: "Pokémon Booster Pack S&V Obsidian Flames Inglés (Arte Aleatorio)",
+    images: [
+       "https://acdn-us.mitiendanube.com/stores/001/989/991/products/pokemon_tcg_scarlet_violetobsid-dd8fdb7a4964322d0a17451143424423-1024-1024.png"
+    ],
+    priceCurrent: "$7.650,00",
+    priceOld: "$8.500,00",
+    discount: "-10% OFF",
+    id: "199652516"
+  },
+  {
+    link: "https://www.shibuyacomicstore.com.ar/productos/pokemon-booster-pack-sv-journey-together-ingles-arte-aleatorio/",
+    title: "Pokémon Booster Pack S&V Obsidian Flames Inglés (Arte Aleatorio)",
+    images: [
+       "https://acdn-us.mitiendanube.com/stores/001/989/991/products/pokemon_tcg_scarlet_violetobsid-2-11e0a9ba4c6917720617451143453318-1024-1024.png"
+    ],
+    priceCurrent: "$7.650,00",
+    priceOld: "$8.500,00",
+    discount: "-10% OFF",
+    id: "199652517"
+    }
+];
